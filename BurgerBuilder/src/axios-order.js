09aa9c13a0ebad58f5735e31438f4369331562ca
Naman <amanarora19599@gmail.com>
2023-09-03 23:34:23 +0530

    Code Push Project In Progress

--- BurgerBuilder/src/axios-order.js
+++ BurgerBuilder/src/axios-order.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger-4154f-default-rtdb.firebaseio.com'
+    // baseURL: 'https://react-my-burger-4154f-default-rtdb.firebaseio.com'
+    baseURL : 'https://react-my-burger-4154f-default-rtdb.firebaseio.com'
 
 })
 
